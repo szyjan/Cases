c977b98472dd6f8f3fb1d83593545e9aadf07506
Szymon <Szymon@DESKTOP-SD47HEE>
2022-05-28 07:58:33 +0200

    Add project files.

---
+++ src/Cases.Api/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.Run();
